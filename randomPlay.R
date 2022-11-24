10da7147aa92933484e8715b36118e76a9ffe9c7
thmichaellin <117919865+thmichaellin@users.noreply.github.com>
2022-11-24 15:16:58 +0100

    Delete randomPlay.R

--- randomPlay.R
+++
@@ -1,6 +0,0 @@
-randomPlay <- function(field){
-  posRow <- as.numeric(which(field == 0, arr.ind=T)[1,][1])
-  posCol <- as.numeric(which(field == 0, arr.ind=T)[1,][2])
-  field[posRow, posCol] <<- +1
-}
-
